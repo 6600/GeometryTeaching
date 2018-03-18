637f1ea5cb8fd4b9453236911b493e29629f270d
PUGE <100284685@qq.com>
2018-03-18 13:32:52 +0800

    完善盒子折纸

---
+++ src/components/Order.js
@@ -0,0 +1,56 @@
+const THREE = require('three')
+const Fun = {
+  init3D (DOM) {
+    return new Promise((resolve, reject) => {
+      const WIDTH = window.innerWidth
+      const HEIGHT = window.innerHeight
+      // 创建场景
+      let scene = new THREE.Scene()
+      // 配置渲染器
+      let renderer = new THREE.WebGLRenderer({ antialias: true })
+      // 设置渲染器大小
+      renderer.setSize(WIDTH, HEIGHT)
+      // 设置canvas背景色(clearColor)和背景色透明度（clearAlpha）
+      renderer.setClearColor(0x333F47, 1)
+      // 添加阴影
+      renderer.shadowMap.enabled = true
+      // 添加软阴影
+      renderer.shadowMapSoft = true
+      // 将渲染DOM追加到页面上
+      DOM.appendChild(renderer.domElement)
+      // 配置 透视 相机
+      // PerspectiveCamera(fov, aspect, near, far)
+      // Fov – 相机的视锥体的垂直视野角
+      // Aspect – 相机视锥体的长宽比
+      // Near – 相机视锥体的近平面
+      // Far – 相机视锥体的远平面
+      let camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 1, 1000)
+      // 设置相机位置
+      camera.position.set(-2, 2, 6)
+      // 指定它看着原点方向
+      camera.lookAt(scene.position)
+      // 给场景追加相机
+      scene.add(camera)
+
+      // --------------------------- 创建光照 ---------------------------
+      // 方向光源
+      let object3d = new THREE.DirectionalLight('white', 1)
+      object3d.position.set(0, 0, 6)
+      object3d.name = 'Back light'
+      scene.add(object3d)
+      // 方向光源
+      object3d = new THREE.DirectionalLight('white', 1)
+      object3d.position.set(0, 0, -6)
+      object3d.name = 'Key light'
+      scene.add(object3d)
+      // 方向光源
+      object3d = new THREE.DirectionalLight('white', 1)
+      object3d.position.set(-2, 6, 0)
+      object3d.name = 'Key light'
+      scene.add(object3d)
+      resolve({scene, renderer, camera})
+    })
+  }
+}
+
+export { Fun }
---
+++ src/components/origami/cube-one.vue
@@ -0,0 +1,113 @@
+<template lang="pug">
+  .cube-one
+</template>
+
+<script>
+import { Fun } from '@/components/Order.js'
+const THREE = require('three')
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      camera: null,
+      renderer: null,
+      spiale: [],
+      meshs: [],
+      step: 0
+    }
+  },
+  mounted () {
+    Fun.init3D(this.$el).then((Object3D) => {
+      this.camera = Object3D.camera
+      this.renderer = Object3D.renderer
+      this.scene = Object3D.scene
+      this.creatCube(Object3D.scene, Object3D.renderer, Object3D.camera)
+    })
+  },
+  methods: {
+    nextStep () {
+      setTimeout(() => {
+        this.step++
+        this.animation(this.step)
+        this.renderScene()
+      }, 20)
+    },
+    renderScene () {
+      this.renderer.render(this.scene, this.camera)
+    },
+    animation (step) {
+      const spiale = this.spiale
+      if (step <= 90) {
+        // 盒子左1
+        spiale[0].rotation.y = step * (Math.PI / 180)
+        spiale[1].rotation.y = step * (Math.PI / 180)
+        // 盒子右部
+        spiale[3].rotation.y = -step * (Math.PI / 180)
+        // 盒子上部
+        spiale[4].rotation.x = step * (Math.PI / 180)
+        // 盒子下部
+        spiale[5].rotation.x = -step * (Math.PI / 180)
+        this.nextStep()
+      } else if (step === 91) {
+        // 重设0面转轴
+        this.meshs[0].position.set(-0.5, 0, 0)
+        this.spiale[0].position.set(-0.5, 0, 1)
+        spiale[0].rotation.y = step * (Math.PI / 180)
+        this.nextStep()
+      } else if (step < 181) {
+        spiale[0].rotation.y = step * (Math.PI / 180)
+        this.nextStep()
+      } else {
+        console.log('动画已播放完毕!')
+      }
+    },
+    creatCube (scene, renderer, camera) {
+      // 创建正方体的6个平面
+      // 定义长宽都是1平面
+      const geometry = new THREE.PlaneGeometry(1, 1)
+      // 定义6个颜色
+      const colors = ['#64e530', '#ccaa1f', '#6b63ef', '#f6c161', '#f46f4c', '#c9b5ec']
+      // 定义6个坐标
+      const positions = [[-1.5, 0, 0], [-0.5, 0, 0], [0, 0, 0], [0.5, 0, 0], [0, 0.5, 0], [0, -0.5, 0]]
+      // 定义6个转轴
+      const axiss = [[-0.5, 0, 0], [-0.5, 0, 0], [0, 0, 0], [0.5, 0, 0], [0, 0.5, 0], [0, -0.5, 0]]
+      // 创造6个平面
+      for (let index in colors) {
+        // 取得颜色
+        const color = colors[index]
+        // 取得坐标
+        const position = positions[index]
+        // 取得转轴
+        const axis = axiss[index]
+        // 创建转轴
+        this.spiale[index] = new THREE.Object3D()
+        this.spiale[index].position.set(axis[0], axis[1], axis[2])
+        this.scene.add(this.spiale[index])
+        this.meshs[index] = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
+          color: color,
+          transparent: true,
+          // 双面双面贴图
+          side: THREE.DoubleSide
+        }))
+        // 设置平面位置
+        this.meshs[index].position.set(position[0], position[1], position[2])
+        // 设置平面角度
+        this.meshs[index].rotation.set(0, 0, 0)
+        // 设置平面缩放
+        this.meshs[index].scale.set(1, 1, 1)
+        // 取消面剔除
+        this.meshs[index].doubleSided = true
+        this.meshs[index].castShadow = true
+        // 将平面添加到场景中
+        this.scene.add(this.meshs[index])
+        this.spiale[index].add(this.meshs[index])
+      }
+      // 调试转轴
+      this.spiale[0].add(new THREE.AxesHelper(50))
+      setTimeout(() => {
+        this.nextStep()
+      }, 0)
+    }
+  }
+}
+</script>
