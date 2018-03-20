4d977f06bdf543d7cdc3735e665fe43c09c2443a
mrzhangfelix <805908873@qq.com>
2018-03-20 20:48:33 +0800

    p老板就是牛逼啊让我成功完成了重置功能

--- src/components/game/activity.vue
+++ src/components/game/activity.vue
@@ -4,7 +4,7 @@
       span.title 點擊適當的方格來塗色，設計正方體的折紙圖樣。
       .block-box
         .blockrow(v-for="i in 5")
-          .block(v-for="j in 5", @click="blockClick(i-1, j-1, $event)", :class="{ active: blocks[i-1][j-1] }")
+          .block(v-for="j in 5", @click="blockClick(i-1, j-1)", :class="{ active: blocks[i-1][j-1] }")
         .clear
       .game-menu
         Line110(@onClick="reset",text="重設")
@@ -28,13 +28,13 @@ export default {
     return data
   },
   methods: {
-    blockClick (i, j, event) {
+    blockClick (i, j) {
       this.$set(this.blocks[i], [j], !this.blocks[i][j])
     },
     reset () {
       for (var i = 0; i < 5; i++) {
         for (var j = 0; j < 5; j++) {
-          this.$set(this.blocks[i][j] = false)
+          this.$set(this.blocks[i], [j], false)
         }
       }
     }
