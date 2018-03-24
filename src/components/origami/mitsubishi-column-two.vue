e74b8f1404941f59eeea9f2128702b8fe55420bb
mrzhangfelix <805908873@qq.com>
2018-03-24 20:31:13 +0800

    三棱柱2的重新绘制

--- src/components/origami/mitsubishi-column-two.vue
+++ src/components/origami/mitsubishi-column-two.vue
@@ -36,13 +36,25 @@ export default {
     },
     creatMitsubishiColumn (scene, renderer, camera) {
       // 定义长宽都是1平面
-      const geometry = new THREE.PlaneGeometry(1, 1.2)
+      const geometry = new THREE.PlaneGeometry(1, Math.sqrt(3))
+      let shape1 = new THREE.Shape()
+      shape1.moveTo(0, 0)
+      shape1.lineTo(1, 0)
+      shape1.lineTo(0.5, Math.sqrt(3) / 2)
+      shape1.lineTo(0, 0)
+      let triangleGeometry1 = new THREE.ShapeGeometry(shape1)
+      let shape2 = new THREE.Shape()
+      shape2.moveTo(0, 0)
+      shape2.lineTo(1, 0)
+      shape2.lineTo(0.5, -Math.sqrt(3) / 2)
+      shape2.lineTo(0, 0)
+      let triangleGeometry2 = new THREE.ShapeGeometry(shape2)
       // 定义6个颜色
-      const colors = ['#64e530', '#ccaa1f', '#6b63ef']
+      const colors = ['#64e530', '#ccaa1f', '#6b63ef', '#f6c161', '#f46f4c']
       // 定义6个坐标
-      const positions = [[-1.5, 0, 0], [0, 0, 0], [1.5, 0, 0]]
+      const positions = [[0.25, 3 * Math.sqrt(3) / 4, 0], [-0.25, 3 * Math.sqrt(3) / 4, 0], [0.75, 3 * Math.sqrt(3) / 4, 0], [-0.5, 0, 0], [1.5, Math.sqrt(3), 0]]
       // 定义6个转轴
-      const axiss = [[-0.5, 0, 0], [0, 0, 0], [0, 0, 0]]
+      const axiss = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
       // 创造6个平面
       for (let index in colors) {
         // 取得颜色
@@ -55,12 +67,28 @@ export default {
         this.spiale[index] = new THREE.Object3D()
         this.spiale[index].position.set(axis[0], axis[1], axis[2])
         this.scene.add(this.spiale[index])
-        this.meshs[index] = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
-          color: color,
-          transparent: true,
-          // 双面双面贴图
-          side: THREE.DoubleSide
-        }))
+        if (index === '3') {
+          this.meshs[index] = new THREE.Mesh(triangleGeometry1, new THREE.MeshLambertMaterial({
+            color: color,
+            transparent: true,
+            // 双面双面贴图
+            side: THREE.DoubleSide
+          }))
+        } else if (index === '4') {
+          this.meshs[index] = new THREE.Mesh(triangleGeometry2, new THREE.MeshLambertMaterial({
+            color: color,
+            transparent: true,
+            // 双面双面贴图
+            side: THREE.DoubleSide
+          }))
+        } else {
+          this.meshs[index] = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
+            color: color,
+            transparent: true,
+            // 双面双面贴图
+            side: THREE.DoubleSide
+          }))
+        }
         // 设置平面位置
         this.meshs[index].position.set(position[0], position[1], position[2])
         // 设置平面角度
@@ -74,23 +102,13 @@ export default {
         this.scene.add(this.meshs[index])
         this.spiale[index].add(this.meshs[index])
       }
-      const triangle = [
-        {
-          spot: [[-0.5, 0.5, 0], [0, 1.5, 0], [0.5, 0.5, 0]],
-          color: 0x0000ff
-        },
-        {
-          spot: [[-0.5, -0.5, 0], [0, -1.5, 0], [0.5, -0.5, 0]],
-          color: 0x0000ff
-        }
-      ]
-      Fun.drawTriangle(triangle).then((mesh) => {
-        // 将平面添加到场景中
-        scene.add(...mesh)
-      })
+      this.spiale[4].add(new THREE.AxesHelper(50))
       setTimeout(() => {
+        this.spiale[0].rotation.z = 60 * (Math.PI / 180)
+        this.spiale[1].rotation.z = -60 * (Math.PI / 180)
+        this.spiale[2].rotation.z = -60 * (Math.PI / 180)
         this.nextStep()
-      }, 1000)
+      }, 1)
     }
   }
 }
