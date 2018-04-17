83ea77a08228053fd8e4876cd7b8a286ac77ab27
mrzhangfelix <805908873@qq.com>
2018-04-17 09:43:09 +0800

    三棱锥2

--- src/components/pyramid/three-two.vue
+++ src/components/pyramid/three-two.vue
@@ -15,7 +15,7 @@ export default {
       meshs: [],
       pause: false,
       step: 0,
-      stepCount: 420
+      stepCount: 334
     }
   },
   mounted () {
@@ -69,19 +69,16 @@ export default {
     close (step) {
       const spiale = this.spiale
       const ratio = (Math.PI / 180)
-      if (step <= 90) {
+      if (step <= 117) {
         // 盒子左1
-        spiale[3].rotation.x = step * ratio
+        spiale[1].rotateY(-ratio)
         this.closeBox()
-      } else if (step <= 180) {
+      } else if (step <= 234) {
         // 上部
-        spiale[4].rotation.x = -(step - 90) * ratio
+        spiale[2].rotateY(ratio)
         this.closeBox()
-      } else if (step <= 300) {
-        spiale[0].rotation.y = (step - 180) * ratio
-        this.closeBox()
-      } else if (step <= 420) {
-        spiale[2].rotation.y = -(step - 300) * ratio
+      } else if (step <= 334) {
+        spiale[3].rotateX(-ratio)
         this.closeBox()
       } else {
         // 广播关闭完成事件
@@ -92,25 +89,22 @@ export default {
     open (step) {
       const spiale = this.spiale
       const ratio = (Math.PI / 180)
-      if (step < 0) {
+      if (step <= 0) {
         // 广播关闭完成事件
         this.$emit('OpenFinish')
         console.log('动画已播放完毕!')
         return
       }
-      if (step <= 90) {
+      if (step > 234) {
         // 盒子左1
-        spiale[3].rotation.x = step * ratio
+        spiale[3].rotateX(ratio)
         this.openBox()
-      } else if (step <= 180) {
+      } else if (step > 117) {
         // 上部
-        spiale[4].rotation.x = -(step - 90) * ratio
+        spiale[2].rotateY(-ratio)
         this.openBox()
-      } else if (step <= 300) {
-        spiale[0].rotation.y = (step - 180) * ratio
-        this.openBox()
-      } else if (step <= 420) {
-        spiale[2].rotation.y = -(step - 300) * ratio
+      } else if (step > 0) {
+        spiale[1].rotateY(ratio)
         this.openBox()
       }
     },
@@ -149,10 +143,11 @@ export default {
       this.dragClose(step)
     },
     creatMitsubishiColumn (scene, renderer, camera) {
+      // const ratio = Math.PI / 180
       let shape1 = new THREE.Shape()
       shape1.moveTo(0, 0)
       shape1.lineTo(1, 0)
-      shape1.lineTo(0.5, Math.sqrt(3) / 2)
+      shape1.lineTo(0.5, Math.sqrt(3))
       shape1.lineTo(0, 0)
       let triangleGeometry1 = new THREE.ShapeGeometry(shape1)
       let shape2 = new THREE.Shape()
@@ -163,22 +158,22 @@ export default {
       let triangleGeometry2 = new THREE.ShapeGeometry(shape2)
       let shape3 = new THREE.Shape()
       shape3.moveTo(0, 0)
-      shape3.lineTo(-0.5, 0)
-      shape3.lineTo(-2.5, 1)
-      shape3.lineTo(0, Math.sqrt(3) / 2)
+      shape3.lineTo(Math.cos(Math.PI / 2 - Math.acos(0.5 / Math.sqrt(3.25))), Math.sin(Math.PI / 2 - Math.acos(0.5 / Math.sqrt(3.25))))
+      shape3.lineTo(0, Math.sqrt(3 + 0.25))
+      shape3.lineTo(0, 0)
       let triangleGeometry3 = new THREE.ShapeGeometry(shape3)
       let shape4 = new THREE.Shape()
       shape4.moveTo(0, 0)
-      shape4.lineTo(0.5, 0)
-      shape4.lineTo(2.5, 1)
-      shape4.lineTo(0, Math.sqrt(3) / 2)
+      shape4.lineTo(-Math.cos(Math.PI / 2 - Math.acos(0.5 / Math.sqrt(3.25))), Math.sin(Math.PI / 2 - Math.acos(0.5 / Math.sqrt(3.25))))
+      shape4.lineTo(0, Math.sqrt(3 + 0.25))
+      shape4.lineTo(0, 0)
       let triangleGeometry4 = new THREE.ShapeGeometry(shape4)
       // 定义3个颜色
       const colors = ['#64e530', '#ccaa1f', '#6b63ef', '#f6c161']
       // 定义3个坐标
-      const positions = [[0, 0, 0], [0, 0, 0], [-0.5, 0, 0], [-0.5, 0, 0]]
+      const positions = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
       // 定义3个转轴
-      const axiss = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+      const axiss = [[0, 0, 0], [1, 0, 0], [0, 0, 0], [0, 0, 0]]
       // 创造3个平面
       for (let index in colors) {
         // 取得颜色
@@ -191,7 +186,7 @@ export default {
         this.spiale[index] = new THREE.Object3D()
         this.spiale[index].position.set(axis[0], axis[1], axis[2])
         this.scene.add(this.spiale[index])
-        if (index === '2') {
+        if (index === '0') {
           this.meshs[index] = new THREE.Mesh(triangleGeometry1, new THREE.MeshPhongMaterial({
             color: color,
             transparent: true,
@@ -205,14 +200,14 @@ export default {
             // 双面双面贴图
             side: THREE.DoubleSide
           }))
-        } else if (index === '0') {
+        } else if (index === '1') {
           this.meshs[index] = new THREE.Mesh(triangleGeometry3, new THREE.MeshPhongMaterial({
             color: color,
             transparent: true,
             // 双面双面贴图
             side: THREE.DoubleSide
           }))
-        } else if (index === '1') {
+        } else if (index === '2') {
           this.meshs[index] = new THREE.Mesh(triangleGeometry4, new THREE.MeshPhongMaterial({
             color: color,
             transparent: true,
@@ -235,6 +230,8 @@ export default {
         this.spiale[index].add(this.meshs[index])
       }
       // this.spiale[1].add(new THREE.AxesHelper(50))
+      this.spiale[2].rotation.z = -Math.PI / 2 + Math.acos(0.5 / Math.sqrt(3.25))
+      this.spiale[1].rotation.z = Math.PI / 2 - Math.acos(0.5 / Math.sqrt(3.25))
       setTimeout(() => {
         this.renderScene()
       }, 0)
