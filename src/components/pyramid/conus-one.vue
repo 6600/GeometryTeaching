fde7fefce69fdb655c6cb117140eb00910a7723e
PUGE <100284685@qq.com>
2018-06-24 00:05:25 +0800

    save

--- src/components/pyramid/conus-one.vue
+++ src/components/pyramid/conus-one.vue
@@ -3,10 +3,9 @@
 </template>
 
 <script>
-import { Fun, Order } from '@/components/Order.js'
+import { Fun } from '@/components/Order.js'
 const THREE = require('three')
 const OrbitControls = require('three-orbit-controls')(THREE)
-const stepSave = require('@/assets/step/conus.json')
 export default {
   name: 'conusOne',
   data () {
@@ -15,10 +14,10 @@ export default {
       renderer: null,
       scene: null,
       spiale: [],
+      controls: null,
       meshs: [],
       step: 0,
-      controls: null,
-      stepCount: 90
+      stepCount: 92
     }
   },
   mounted () {
@@ -28,13 +27,6 @@ export default {
       this.scene = Object3D.scene
       this.creatCube(Object3D.scene, Object3D.renderer, Object3D.camera)
     })
-    // 监听暂停事件
-    Order.$on(`pause`, () => {
-      this.pause = true
-    })
-  },
-  beforeDestroy () { // 移除监听
-    Order.$off('pause')
   },
   methods: {
     animate () {
@@ -58,52 +50,51 @@ export default {
     renderScene () {
       this.renderer.render(this.scene, this.camera)
     },
-    getStep (step) {
-      if (step <= 0) {
-        this.$emit('OpenFinish')
-        console.log('动画已播放完毕!')
-        return
-      }
-      // console.log(step)
-      if (step > this.stepCount) {
-        // 广播关闭完成事件
-        this.$emit('CloseFinish')
-        console.log('动画已播放完毕!')
-        return
-      }
-      if (step === this.stepCount) {
-        // 广播关闭完成事件
-        this.$emit('CloseFinish')
-        console.log('动画已播放完毕!')
-      }
-      const spiale = this.spiale
-      const vLength = 41 // this.meshs[1].geometry.vertices.length / 2
-      // console.log(stepSave)
-      spiale[1].rotation.x = -step * (Math.PI / 180)
-      // -----------------------------
-      for (let i = 0; i < vLength; i++) {
-        this.meshs[0].geometry.vertices[i].z = stepSave[step][i].z
-        this.meshs[0].geometry.vertices[i + 41].z = stepSave[step][i + 41].z
-        this.meshs[0].geometry.vertices[i].x = stepSave[step][i].x
-        this.meshs[0].geometry.vertices[i + 41].x = stepSave[step][i + 41].x
-        this.meshs[0].geometry.vertices[i].y = stepSave[step][i].y
-        this.meshs[0].geometry.vertices[i + 41].y = stepSave[step][i + 41].y
-      }
-      this.meshs[0].geometry.verticesNeedUpdate = true
-      return true
-    },
     close (step) {
-      if (this.getStep(step)) this.nextStep(1, this.close)
-    },
-    open (step) {
-      if (step > this.stepCount) step = this.stepCount
-      if (this.getStep(step)) this.nextStep(-1, this.open)
-    },
-    dragClose (step) {
-      if (this.getStep(step)) this.renderScene()
-    },
-    dragOpen (step) {
-      if (this.getStep(step)) this.renderScene()
+      const spiale = this.spiale
+      // console.log(this.meshs[0].geometry.vertices)
+      // return
+      if (step <= 91) {
+        spiale[1].rotation.x = -step * (Math.PI / 180)
+        // 因为分为上下两个顶点所以要除2
+        const vLength = this.meshs[0].geometry.vertices.length / 2
+        // console.log(vLength)
+        // -----------------------------
+        for (let i = 0; i <= vLength - 1; i++) {
+          const Z = Math.abs(i - 20) * 0.05
+          let X = null
+          let X2 = null
+          const Y = 2 - Math.sqrt(4 - Math.pow(Math.abs(i - 20) * Math.PI / 40, 2))
+          // console.log(i, Y)
+          // return
+          this.meshs[0].geometry.vertices[i].y -= Y / 90
+          this.meshs[0].geometry.vertices[i + 41].y -= Y / 90
+          // 中心点慢慢向中心偏移
+          // this.meshs[0].geometry.vertices[20].z += 1 / 90
+          // this.meshs[0].geometry.vertices[61].z += 1 / 90
+          if (i < 20) {
+            X = -Math.sqrt(0.25 - Math.pow(Z - 0.5, 2)) + (Math.abs(i - 20) * Math.PI / 40)
+            this.meshs[0].geometry.vertices[i].x += X / 90 + Math.sqrt(0.25 - Math.pow(Z - 0.5, 2)) / 90
+          } else {
+            X = Math.sqrt(0.25 - Math.pow(Z - 0.5, 2)) - (Math.abs(i - 20) * Math.PI / 40)
+            this.meshs[0].geometry.vertices[i].x += X / 90 - Math.sqrt(0.25 - Math.pow(Z - 0.5, 2)) / 90
+          }
+          // console.log((20 - Math.abs(i - 20)))
+          this.meshs[0].geometry.vertices[i].z = Z / 90 * step + (step * (20 - Math.abs(i - 20)) / 40 / 90)
+          this.meshs[0].geometry.vertices[i + 41].z = Z / 90 * step 
+          
+          this.meshs[0].geometry.vertices[i + 41].x += X / 90
+        }
+        // console.log(this.meshs[0].geometry.vertices)
+        // return
+        this.meshs[0].geometry.verticesNeedUpdate = true
+        this.nextStep(1, this.close)
+      } else {
+        var geometry = new THREE.ConeBufferGeometry( 0.5, 1, 100 );
+        this.meshs[0].geometry = geometry
+        this.spiale[0].position.set(0, 0.04, 0.5)
+        console.log(geometry)
+      }
     },
     creatCube (scene, renderer, camera) {
       this.controls = new OrbitControls(this.camera)
