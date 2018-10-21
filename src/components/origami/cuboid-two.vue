1ad796d758fd3abbbeafedd6d37990f0be7325a4
PUGE <100284685@qq.com>
2018-10-22 00:21:01 +0800

    更换新模型

--- src/components/origami/cuboid-two.vue
+++ src/components/origami/cuboid-two.vue
@@ -5,7 +5,7 @@
 <script>
 import { Fun, Order } from '@/components/Order.js'
 const THREE = require('three')
-const OrbitControls = require('three-orbit-controls')(THREE)
+const OrbitControls = require('@puge/three-orbit-controls')(THREE)
 export default {
   name: 'cuboid-two',
   data () {
--- src/components/origami/hexagonal-prism-two.vue
+++ src/components/origami/hexagonal-prism-two.vue
@@ -5,7 +5,7 @@
 <script>
 import { Fun, Order } from '@/components/Order.js'
 const THREE = require('three')
-const OrbitControls = require('three-orbit-controls')(THREE)
+const OrbitControls = require('@puge/three-orbit-controls')(THREE)
 const stepSave = require('@/assets/step/hexagonal-prism-two.json')
 export default {
   name: 'HelloWorld',
